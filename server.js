102940f37b3e749ed7d391f42b142fa7d8708030
Manav Chhuneja <manavchhuneja@Manavs-Mac-mini.local>
2023-08-01 00:04:08 -0400

    Changing layout and setting up coin array variables

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import axios from "axios";
 import { config } from "dotenv";
+import fs from "fs";
 config();
 
 const app = express();
 const port = 3000;
+const topCoins = JSON.parse(fs.readFileSync("./popularCoins.json", "utf-8"));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
